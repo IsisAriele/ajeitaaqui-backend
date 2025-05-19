b7fce6c0a3866ff8a48b4eddfcaba82589820ab7
Isis Ariele Araújo Duarte <isisariele1@gmail.com>
2025-05-18 21:06:13 -0300

    feat: add login with jwt

--- apps/interface_adapters/api/v1/urls.py
+++ apps/interface_adapters/api/v1/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from apps.interface_adapters.api.v1.views.authentication.create_client_view import ClientCreateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("register/", ClientCreateView.as_view(), name="create-client"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
